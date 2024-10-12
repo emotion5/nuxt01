b11ce8f06d752a89d358cab94076197600f917e3
dani <emotion6172@gmail.com>
2024-10-12 11:00:57 +0900

    1st

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    
+    <SimpleDataBinding />
+    
+  </div>
+</template>
+
+<script setup>
+import SimpleDataBinding from '~/components/SimpleDataBinding.vue'
+</script>
---
+++ components/SimpleDataBinding.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>   
+    <p>{{ message }}</p>    
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const message = ref('안녕하세요, Vue!')
+</script>
+
